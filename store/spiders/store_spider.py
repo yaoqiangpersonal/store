96991dbad850bf100f1bbcb04d9399c04917ea9c
yaoqiang <yaoqiangpersonal@gmail.com>
2021-12-07 17:08:15 +0800

    commit

--- store/spiders/store_spider.py
+++ store/spiders/store_spider.py
@@ -8,9 +8,9 @@ class StoreSpiderSpider(scrapy.Spider):
 
     def start_requests(self):
         
-        for page in range(1,4):
+        for page in range(1,3):
             yield scrapy.Request(
-                "https://store.hmv.com/store/promotions/3for_304kultrahd?sort=most_relevant%20desc&quantity=24&view=grid&categories=offers&offers=3%2Bfor%2B%C2%A330%2B4K%2BUltra%2BHD&page=" + str(page),
+                "https://store.hmv.com/store/promotions/2for_244kultrahd?sort=most_relevant%20desc&quantity=24&view=grid&categories=offers&offers=2%2Bfor%2B%C2%A324%2B4K%2BUltra%2BHD&page=" + str(page),
                                 meta={"proxy": self.proxy},
                                 callback=self.parse
                                 )
@@ -30,6 +30,7 @@ class StoreSpiderSpider(scrapy.Spider):
             barcode = response.css('.prod-info__sku span::text').get()
             details = response.css('.table-responsive td::text').getall()
             dic = dict()
+            dic['url'] = url
             dic['name'] = id
             dic['price'] = price
             dic['barcode'] = barcode
